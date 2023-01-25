3c17e5aac8b2e9fb6e3db54045871cd98f679ff2
unknown <k.jones45@yahoo.com>
2023-01-25 15:47:06 -0500

    deleted and made a new human.py file.

--- human.py
+++ human.py
@@ -1,14 +1,12 @@
 # child class
+
 from player import Player
 
 class Human(Player):
-    def __init__(self,name):
+    def __init__(self, name):
         super().__init__(name)
         pass
-        
-    def choose_gesture():
-        user_input = input("Press 1 for Rock \n Press 2 for Paper \n Press 3 for Scissors \n Press 4 for Lizard \n Press 5 for Spock"):
+    def choose_gesture(self):
+        user_input = input("Press 1 for Rock \n Press 2 for Paper \n Press 3 for Scissors \n Press 4 for Lizard \n Press 5 for Spock.")
+        print(user_input)
         pass
-
-
-
