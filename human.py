cc7ca4e2a314d9e0fefac4d3ab40ca900a68486d
unknown <k.jones45@yahoo.com>
2023-01-25 23:15:57 -0500

    working on choose gesture method on human.py. see if print statements can be used into console.

--- human.py
+++ human.py
@@ -8,5 +8,14 @@ class Human(Player):
         pass
     def choose_gesture(self):
         user_input = input("Press 1 for Rock \n Press 2 for Paper \n Press 3 for Scissors \n Press 4 for Lizard \n Press 5 for Spock.")
-        print(user_input)
+        if user_input == "1": 
+            print(f'{self.name} chose Rock')
+        elif user_input == "2":
+            print(f'{self.name} chose Paper')
+        elif user_input == "3":
+            print(f'{self.name} chose Scissors')
+        elif user_input == "4":
+            print(f'{self.name} chose Lizard')
+        elif user_input == "5":
+            print(f'{self.name} chose Spock')
         pass
